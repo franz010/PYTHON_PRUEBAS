ab39acb776940499d71f30ae0b803958472752bd
franz010 <mikilinpachecoch@gmail>
2026-08-03 16:21:18 -0400

    camibios realizados

--- PRUEBITA.py
+++ PRUEBITA.py
@@ -1,1 +1,12 @@
-#hola
+#CREANDO UNA LISTA EN PYTHON
+
+lista = ["miki", "yo,", "Luis", "Mario"]
+
+
+
+frutas = ["manzana", "pera", "banana"]
+frutas[0] = "sandia"
+frutas.append("kiwi")
+frutas.remove("pera")
+
+print(frutas)
